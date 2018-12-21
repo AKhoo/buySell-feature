9669d5b3d3c7b3b76f8ba93c273f74ac21059741
Chris Caden <hellocaden@gmail.com>
2018-12-21 11:02:38 -0800

    completed set up of save function in mongo db

--- database/index.js
+++ database/index.js
@@ -22,13 +22,13 @@ let transactionSchema = mongoose.Schema({
     type: String,
     required: [true, 'date field is required']
   },
-  status: {
-    type: String,
-    required: [true, 'status field is required']
-  },
   quantity: {
     type: Number,
     required: [true, 'quantity field is required']
+  },
+  status: {
+    type: String,
+    required: [true, 'status field is required']
   },
   filled: {
     type: String
@@ -63,12 +63,41 @@ let stockSchema = mongoose.Schema({
 let Transaction = mongoose.model('Transaction', transactionSchema);
 let Stock = mongoose.model('Stock', stockSchema);
 
+function getDateAndTime(){
+  // get current date
+  var date = new Date();
+  // set date to string
+  var dateStr = date.toDateString();
+  // get current time as string
+  var time = date.toLocaleTimeString();
+  // return full string
+  return dateStr + ' ' + time;
+}
 
-
-let save = () => {
-
+let save = (transaction, cb) => {
+  var newTransaction = {
+    stockName: transaction.stockName,
+    stockTicker: transaction.stockTicker,
+    orderType: 'market',
+    timeInForce: 'GFD',
+    date: getDateAndTime(),
+    quantity: transaction.quantity,
+    status: 'pending',
+    filled: 'TBD',
+    filledQuantity: 0,
+    totalCost: 0.00
+  }
+  Transaction.update({'stockName':newTransaction.stockName},
+  {'stockTicker':newTransaction.stockTicker},{'orderType':newTransaction.orderType},
+  {'timeInForce':newTransaction.timeInForce},{'date':newTransaction.date},
+  {'quantity':newTransaction.quantity},{$set:{status:newTransaction.status}},
+  {$set:{filled:newTransaction.filled}},{$set:{filledQuantity:newTransaction.filledQuantity}},
+  {$set:{totalCost:newTransaction.totalCost}},
+  {upsert:true}).then((user) => {
+    cb(null, user);
+  })
 };
 
-
+// RUN THIS CODE TO SEED MONGO DATABASE
 // mongoimport --db robinsHood --collection stocks --file nasdaq-data.csv --type csv --headerline
 module.exports.save = save;
