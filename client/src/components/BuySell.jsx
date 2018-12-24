deea00e8bb9852f2e63c7af5da1ecbc320363e1d
Chris Caden <hellocaden@gmail.com>
2018-12-23 18:05:56 -1000

    setting styled-components for buySell

--- client/src/components/BuySell.jsx
+++ client/src/components/BuySell.jsx
@@ -5,30 +5,57 @@ const Form = styled.form`
   margin: 0 auto;
   width: 275.77px;
   height: 488px
-  text-align: center;
+  font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
+  text-align: left;
   border: 1px solid #ddd;
   padding-top: 0px;
   padding-bottom: 90px;
   color: black;
-  background: white;
+  background: #1b1b1d;
 `;
 
-const Title = styled.h2`
+const Header = styled.h2`
+  width: 275.77px;
+  height: 52px
   margin-top: 40px;
   margin-bottom: 70px;
-  font-size: 1.5em;
-  color: black;
-  background-color: white;
+  font-size: 13px;
+  font-weight: 400;
+  letter-spacing: 0.25px;
+  line-height: 19px;
+  font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
+  color: #fff;
+  background-color: #1b1b1d;
+`;
+
+const TitleHeader = styled.div`
+  -webkit-box-align: center;
+  align-items: center;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  margin-bottom: 64px;
+  padding-top: 64px;
+  font-size: 13px;
+  font-weight: 400;
+  letter-spacing: 0.25px;
+  line-height: 19px;
+  font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
+  color: #fff;
+  background-color: #1b1b1d;
 `;
 
 const Button = styled.button`
   font-size: 1.5em;
-  background-color: black;
+  font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
+  background-color: #1b1b1d;
   color: white;
 `;
 
 const Input = styled.input`
   font-size: 1.45em;
+  font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
   border: 1px solid #ddd;
 `;
 
@@ -43,13 +70,15 @@ class BuySell extends React.Component {
     e.preventDefault();
     console.log(`New signup from ${email}`);
   }
-  
+
   render() {
     return (
     <Form onSubmit={this.buyStock}>
-      <Title>
-        Facebook FB
-      </Title>
+      <Header>
+        <TitleHeader>
+          Buy FB
+        </TitleHeader>
+      </Header>
       <Input type="text" name="quantity" />
       <Button>BUY</Button>
     </Form>
