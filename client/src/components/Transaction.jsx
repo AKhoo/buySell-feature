fe238769cf632fd7ad1f185a454b2695170f21ed
Chris Caden <hellocaden@gmail.com>
2019-01-02 00:30:56 -0800

    added click to open for the transaction history

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const TxDivSmall1 = styled.div`
+const TxDivSmall1Closed = styled.div`
 background-color: rgba(0, 0, 0, 0);
 border-bottom-left-radius: 4px;
 border-bottom-right-radius: 4px;
@@ -20,6 +20,38 @@ margin-bottom: 0px;
 margin-left: -24px;
 margin-right: -24px;
 margin-top: 0px;
+text-align: left;
+text-size-adjust: 100%;
+transition-delay: 0s, 0s, 0s, 0s;
+transition-duration: 0.15s, 0.15s, 0.15s, 0.15s;
+transition-property: background, box-shadow, margin-bottom, -webkit-box-shadow;
+transition-timing-function: ease, ease, ease, ease;
+width: 726.656px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxDivSmall1Open = styled.div`
+background-color: rgb(27, 27, 29);
+border-bottom-left-radius: 4px;
+border-bottom-right-radius: 4px;
+border-top-left-radius: 4px;
+border-top-right-radius: 4px;
+box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 4px 1px, rgba(0, 0, 0, 0.6) 0px 3px 24px 0px;
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: block;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 332px;
+letter-spacing: 0.25px;
+line-height: 19px;
+margin-bottom: 12px;
+margin-left: -24px;
+margin-right: -24px;
+margin-top: 0px;
+position: relative;
 text-align: left;
 text-size-adjust: 100%;
 transition-delay: 0s, 0s, 0s, 0s;
@@ -118,7 +150,7 @@ letter-spacing: 0.25px;
 line-height: 19px;
 text-align: left;
 text-size-adjust: 100%;
-width: 62.0625px;
+width: 100px;
 -webkit-box-direction: normal;
 -webkit-box-orient: vertical;
 -webkit-box-pack: center;
@@ -128,18 +160,16 @@ width: 62.0625px;
 const Div5 = styled.div`
 box-sizing: border-box;
 color: rgb(255, 255, 255);
-display: block;
+display: inline;
 font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
 font-size: 13px;
 font-weight: 400;
+height: auto;
 letter-spacing: 0.25px;
 line-height: 19px;
 text-align: left;
 text-size-adjust: 100%;
-transition-delay: 0s;
-transition-duration: 0.3s;
-transition-property: height;
-transition-timing-function: ease;
+width: auto;
 -webkit-box-direction: normal;
 -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 `;
@@ -172,6 +202,7 @@ display: block;
 font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
 font-size: 16px;
 font-weight: 500;
+height: 22px;
 letter-spacing: normal;
 line-height: 22px;
 margin-block-end: 0px;
@@ -184,6 +215,7 @@ margin-right: 0px;
 margin-top: 0px;
 text-align: left;
 text-size-adjust: 100%;
+width: 89px;
 -webkit-box-direction: normal;
 -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 `;
@@ -228,22 +260,240 @@ text-size-adjust: 100%;
 -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 `;
 
-export default function Transaction (props) {
-  return (
-        <TxDivSmall1>
-          <TxDivSmall2>
-            <TxDivSmall3>
-               <TxDivSmall4>
-                <H31>{props.tx.orderType}</H31>
-                {props.tx.date}
-               </TxDivSmall4>
-               <Div7>
-                <H32>{props.tx.status}</H32>
-                <Span1></Span1>
-               </Div7>
-            </TxDivSmall3>
-            <Div5></Div5>
-          </TxDivSmall2>
-        </TxDivSmall1>
-  )
-}
+const TxBtmMainDivUnselected = styled.div`
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: block;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 0px;
+letter-spacing: 0.25px;
+line-height: 19px;
+overflow-x: hidden;
+overflow-y: hidden;
+text-align: left;
+text-size-adjust: 100%;
+transition-delay: 0s;
+transition-duration: 0.3s;
+transition-property: height;
+transition-timing-function: ease;
+width: 678.656px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxBtmMainDivSelected = styled.div`
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: block;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 242px;
+letter-spacing: 0.25px;
+line-height: 19px;
+text-align: left;
+text-size-adjust: 100%;
+transition-delay: 0s;
+transition-duration: 0.3s;
+transition-property: height;
+transition-timing-function: ease;
+width: 678.656px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxBorderDiv = styled.div`
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: inline;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: auto;
+letter-spacing: 0.25px;
+line-height: 19px;
+text-align: left;
+text-size-adjust: 100%;
+width: auto;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxMainBodyDiv = styled.div`
+border-bottom-color: rgb(14, 13, 13);
+border-left-color: rgb(14, 13, 13);
+border-right-color: rgb(14, 13, 13);
+border-top-color: rgb(14, 13, 13);
+border-top-style: solid;
+border-top-width: 1px;
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: block;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 242px;
+letter-spacing: 0.25px;
+line-height: 19px;
+padding-bottom: 24px;
+text-align: left;
+text-size-adjust: 100%;
+transition-delay: 0s;
+transition-duration: 0.15s;
+transition-property: border-color;
+transition-timing-function: ease;
+width: 678.656px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxMainInfoDiv = styled.div`
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: block;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 174px;
+letter-spacing: 0.25px;
+line-height: 19px;
+padding-top: 24px;
+text-align: left;
+text-size-adjust: 100%;
+width: 678.656px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxInfoDiv = styled.div`
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: flex;
+flex-wrap: wrap;
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 150px;
+letter-spacing: 0.25px;
+line-height: 19px;
+text-align: left;
+text-size-adjust: 100%;
+width: 678.656px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const TxStockNameDiv = styled.div`
+box-sizing: border-box;
+color: rgb(255, 255, 255);
+display: block;
+flex-basis: calc(-8px + 33.3333%);
+font-family: "DIN Pro", -apple-system, system-ui, sans-serif;
+font-size: 13px;
+font-weight: 400;
+height: 38px;
+letter-spacing: 0.25px;
+line-height: 19px;
+margin-bottom: 12px;
+margin-left: 0px;
+margin-right: 6px;
+margin-top: 0px;
+text-align: left;
+text-size-adjust: 100%;
+width: 218.203px;
+-webkit-box-direction: normal;
+-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+export default class Transaction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      txOpen: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (this.state.txOpen) {
+      this.setState({
+        txOpen: false
+      })
+    } else {
+      this.setState({
+        txOpen: true
+      })
+    }
+  }
+
+  render() {
+    const justDate = (this.props.tx.date).slice(3, this.props.tx.date.length)
+    const txExpand = this.state.txOpen ?
+    <TxBtmMainDivSelected>
+      <Div5>
+        <TxMainBodyDiv>
+          <TxMainInfoDiv>
+            <TxInfoDiv>
+              <TxStockNameDiv>
+
+              </TxStockNameDiv>
+            </TxInfoDiv>
+          </TxMainInfoDiv>
+        </TxMainBodyDiv>
+      </Div5>
+    </TxBtmMainDivSelected>
+     :
+    <TxBtmMainDivUnselected>
+      <Div5>
+        <TxMainBodyDiv>
+          <TxMainInfoDiv>
+            <TxInfoDiv>
+              <TxStockNameDiv>
+
+              </TxStockNameDiv>
+            </TxInfoDiv>
+          </TxMainInfoDiv>
+        </TxMainBodyDiv>
+      </Div5>
+    </TxBtmMainDivUnselected>
+    const txDivSmall1 = this.state.txOpen ?
+    <TxDivSmall1Open>
+      <TxDivSmall2>
+        <TxDivSmall3 onClick={this.handleClick}>
+          <TxDivSmall4>
+            <H31>{this.props.tx.orderType}</H31>
+            {justDate}
+          </TxDivSmall4>
+          <Div7>
+            <H32>{this.props.tx.status}</H32>
+            <Span1></Span1>
+          </Div7>
+        </TxDivSmall3>
+        {txExpand}
+      </TxDivSmall2>
+    </TxDivSmall1Open>
+     :
+    <TxDivSmall1Closed>
+      <TxDivSmall2>
+        <TxDivSmall3 onClick={this.handleClick}>
+          <TxDivSmall4>
+            <H31>{this.props.tx.orderType}</H31>
+            {justDate}
+          </TxDivSmall4>
+          <Div7>
+            <H32>{this.props.tx.status}</H32>
+            <Span1></Span1>
+          </Div7>
+        </TxDivSmall3>
+        {txExpand}
+      </TxDivSmall2>
+    </TxDivSmall1Closed>
+    return (
+      <div>
+        {txDivSmall1}
+      </div>
+    )
+  }
+};
